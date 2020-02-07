d6f2bcd79988d663feeb1760ee8ee844f230441e
Bharat Tejwani <bharatt@usf.edu>
2020-02-07 16:01:22 -0500

    changes 2

--- Car.cs
+++ Car.cs
@@ -10,7 +10,21 @@ namespace ConsoleApp4
         static void Main(string[] args)
          { 
             Car myObj = new Car();
+            Car myObj2 = new Car();
             Console.WriteLine(myObj.color);
+            Console.WriteLine(myObj2.color);
+        
+    {
+            // class members
+            string color = "red";
+            int maxspeed = 200;
+            void fullThrottle()
+
+                {
+                    Console.WriteLine("The car is going as fast as it can!");
+
+                }
         }
     }
+}
 }
